5fc70d50b4fc0b5abef9355fc74edf9a809b9c2a
Adarsh Tiwari <tadarsh249@gmail.com>
2023-07-07 18:53:15 +0530

    basic backend setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const dotenv=require('dotenv');
+const colors=require('colors');
+const morgan=require('morgan');
+const cors=require('cors');
+const connectDB = require('./config/db');
+
+
+//dotenv config
+dotenv.config();
+
+//mongodb connect
+connectDB();
+
+const app=express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use('/api/v1/test',require('./routes/testRoute'));
+
+const PORT=process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`.bgBlue.white);
+})
